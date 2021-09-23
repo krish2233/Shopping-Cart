5874c8d269f03497e8980bfaec9925f11a268b78
krish2233 <90921677+krish2233@users.noreply.github.com>
2021-09-23 11:07:13 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Book',
+      price: 1400,
+      image: 'https://picsum.photos/id/180/2400/1600',
+    },
+    {
+      id: '2',
+      name: ' Car',
+      price: 2400,
+      image: 'https://picsum.photos/id/111/4400/2656',
+    },
+    {
+      id: '3',
+      name: ' Shoes',
+      price: 1000,
+      image: 'https://picsum.photos/id/21/3008/2008',
+    },
+  ],
+};
+export default data;
